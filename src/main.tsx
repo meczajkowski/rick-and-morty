558cc94bac5554ef27d20cb8dff571dff5b42390
meczajkowski <michal.erik.czajkowski@gmail.com>
2025-01-22 19:44:49 +0100

    feat: install react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Home from "./pages/Home/Home.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
