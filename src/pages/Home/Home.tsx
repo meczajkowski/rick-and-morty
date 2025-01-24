6429b5cc9a00271d52f3c32e8ca8665d27997f73
meczajkowski <michal.erik.czajkowski@gmail.com>
2025-01-24 11:20:53 +0100

    feat: allow users to filter characters

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,26 @@
 import CharacterList from "../../components/Character/CharacterList/CharacterList";
+import SearchInput from "../../components/ui/SearchInput/SearchInput";
+import useFilteredCharacters from "../../api/useFilteredCharacters";
+import "./Home.css";
 
 function Home() {
-  return <CharacterList />;
+  const { characters, searchQuery, setSearchQuery, noResults } =
+    useFilteredCharacters();
+
+  return (
+    <>
+      <SearchInput
+        value={searchQuery}
+        onChange={setSearchQuery}
+        placeholder="Search characters..."
+      />
+      <CharacterList
+        characters={characters}
+        searchQuery={searchQuery}
+        noResults={noResults}
+      />
+    </>
+  );
 }
 
 export default Home;
